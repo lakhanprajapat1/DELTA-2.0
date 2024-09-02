bd3bbbb083b26347a311402903cedec86907a377
Lakhan Prajapat <lakhan10209@gmail.com>
2024-09-02 09:55:03 +0530

    Add files via upload

---
+++ Day_1/first.js
@@ -0,0 +1,13 @@
+const profile={
+    username :"lakhanprajapat",
+    post: 192,
+    followers :598,
+    following:4,
+    name: "lakhan_prajapat",
+    isfollow: true,
+    bio : "apna_college",
+};
+
+console.log(profile);
+console.log(profile["username"]);
+console.log(typeof profile["username"]);
