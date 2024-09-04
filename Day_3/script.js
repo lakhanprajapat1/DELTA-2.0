b10ca2be406628328c6faabadbd4082fb1519127
Lakhan Prajapat <lakhan10209@gmail.com>
2024-09-04 10:18:44 +0530

    Add files via upload

---
+++ Day_3/script.js
@@ -0,0 +1,94 @@
+// console.log("hello coder");
+// switch statement...................
+// let day = "2";
+
+// switch(day){
+//     case "1":
+//         console.log("monday");
+//         break;
+//     case "2":
+//         console.log("tuesday");
+//         break;
+//     case "3":
+//         console.log("wednesday");
+//         break;
+//     case "4":
+//         console.log("thursday");
+//         break;
+//     case "5":
+//         console.log("friday");
+//         break;
+//     case "6":
+//         console.log("saturday");
+//         break;
+//     default:
+//         console.log("sunday");
+// }
+
+//practice qus
+
+// let num= prompt();
+
+// if(num%10==0){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
+
+// let name=prompt("enter your name");
+// let age = prompt("enter your age");
+
+// console.log( name,"is", age, "year old." );
+
+
+// let quater= 2;
+
+// switch (quater) {
+//     case 1:
+//         console.log("january, faburary, march");
+//         break;
+//     case 2:
+//         console.log("april, may, june");
+//         break;
+//     case 3:
+//         console.log("july, august, septembet");
+//         break;    
+//     default:
+//         console.log("october, november, december");
+//         break;
+// }
+
+// let str="mango";
+
+// if((str[0]==="a") && (str.length<=5)){
+//     console.log(str, "is a golden string.");
+// }else{
+//     console.log(str, "is not a golden string !!.");
+// }
+
+// let num1="7";
+// let num2="5";
+// let num3="3";
+
+// if((num1>=num2)&&(num1>=num3)){
+//     console.log(num1, "is largest number");
+// }else if((num2>=num1)&&(num2>=num3)){
+//     console.log(num2,"is largest number");
+// }else{
+//     console.log(num3,"is largest number");
+// }
+
+// define variable
+// let num1=32;
+// let num2=47852;
+
+// // extract the last digit of each number
+// let last1= num1 % 10;
+// let last2= num2 % 10;
+
+// if(last1===last2){
+//     console.log(num1,"and", num2, "have a same last digit is 2");
+// }else{
+//     console.log(num1,"and", num2, "have a not same last digit is 2");
+// }
+
