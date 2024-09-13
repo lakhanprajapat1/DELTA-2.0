f02c25a28653885e254bb56f3fea09e629496d28
Lakhan Prajapat <lakhan10209@gmail.com>
2024-09-13 19:24:49 +0530

    Add files via upload

---
+++ Day_16/practice.js
@@ -0,0 +1,63 @@
+// console.log("practice quations")
+
+// let msg="help!"
+// console.log(msg.toUpperCase().trim())
+
+
+// let name="ApnaCollege";
+// console.log(name.slice(4,9));
+// console.log(name.indexOf("na"));
+// console.log(name.replace("Apna","Our"));
+
+// console.log(name.slice(4).replace("l","t"));
+
+
+// let months=["january", "july","march","august"];
+// let lang=["html","css","javascript","python","c","c++","C#"];
+
+// let result=lang.reverse().indexOf("javascript");
+// console.log(result);
+
+// // tik tak toe 
+// let game=[['X',null,'O'],[null,'X',null],['O',null,'X']];
+
+// // qus 1
+// let arr= [7, 9, 0, -2];
+// let n=3;
+// let result=arr.slice(0, n);
+// console.log(result);
+
+// // qus 2
+// let arr =[7,9,0,-2];
+// let n=3;
+// let result=arr.slice(-n);
+// console.log(result);
+
+// // // qus 3
+// let str=prompt("Enter Your String");
+// if(str.trim().length==0){
+//     console.log(str, "string is blank");
+// }
+// else{
+//     console.log( str ,"string is not blank");
+// }
+
+// // qustion 4
+// let str = prompt("enter your string to cheak it is lowercase or not");
+
+// if(str === str.toLowerCase()){
+//     console.log("string is lower case");
+// }else{
+//     console.log("string is not a lowercase");
+// }
+
+// // //qustion no 5
+// let str="         hello,      world!        "                ;
+// let string=str.trim();
+// console.log(string);
+
+// // question no 6
+let arr = [2,4,6,8,10]
+console.log(arr.indexOf(3)); //-1 because not found
+
+
---
+++ Day_16/script.js
@@ -0,0 +1,72 @@
+console.log("Day_!6 ");
+
+// //trim method..................
+// let msg = "         lak  han        ";
+
+// console.log( msg );
+// console.log( msg.trim() );
+
+// let passward=prompt("enter your passward");
+// let newpassward = passward.trim();
+
+
+// console.log(newpassward);
+
+// //toLowerCase and toUpperCase method...........
+// let name = "Lakhan Prajapati";
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name);
+
+
+// // indexOf method
+
+// let str = "ilovechai";
+// console.log(str.indexOf("e"));
+
+
+// // chaining method.........
+// let msg="       hello coder      ";
+// let newMsg=msg.toUpperCase().trim();
+// console.log(newMsg);
+
+// //slice method...........
+// let msg="hellocoder";
+// console.log(msg.slice(5));
+
+
+// //Replace Method..............
+// let msg="ilovecoding"
+// console.log(msg);
+// console.log(msg.replace("coding","chai"));
+// console.log(msg.replace("l","L"));
+
+// //Repeat method...........
+// let fruit ="mango";
+// console.log(fruit.repeat(5));
+
+// //Array (data structure)........
+// let students=["aman","raman","rohan"];
+// console.log(students);
+
+// let nums=[2,4,6,8];
+// console.log(nums);
+
+
+// //arrays are immutable...
+// let fruits=["mango","apple","litchi"];
+// console.log(fruits);
+// fruits[0]="banan";
+// console.log(fruits);
+
+// //array methods..
+// let cars=["a","b","c"]
+// cars.push("ab");
+// console.log(cars);
+
+// let cars=["audi","bmw","xuv","rollsroysw"];
+// let num=["45","78","100","19","91"];
+
+// nested arrays
+// let nums=[[2,4],[3,6],[4,8]];
