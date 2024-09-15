7ce6cc25a352b5d879f00d9d7919fd50c37b7efb
Lakhan Prajapat <lakhan10209@gmail.com>
2024-09-15 13:26:09 +0530

    Add files via upload

---
+++ Day_17/app.js
@@ -0,0 +1,58 @@
+console.log("Day_17")
+
+// const favCartoon="doremon"
+// let guess=prompt("Enter My Favorate Cartoon")
+
+// while((guess!=favCartoon)&&(guess!="quit")){
+//     guess=prompt("Guess Favorate Cartoon")
+// }
+// if(favCartoon=="doremon"){
+//     console.log("Congratulation!!");
+// }else{
+//     console.log("You Quit!!");
+// }
+
+// let fruits=["apple","mango","banana","litchi","papaya"];
+
+// for(let i=0 ; i <fruits.length ; i++){
+//     console.log(i, fruits[i]);
+// }
+
+// let heros=[['ironman', 'thor', 'spiderman'],['superman', 'wonderman', 'flash']];
+
+// for(let i=0; i<heros.length; i++){
+//     for(let j=0; j<heros[i]; j++){
+//         console.log(heros[i][j]);
+//     }
+// }
+
+
+// let student=[["lakhan", 100], ["amit",98], ["aakash", 79]];
+
+// for(let i=0; i<student.length; i++){
+//     console.log(`student no ${i+1}`);
+//     for(let j=0; j<student[i].length; j++){
+//         console.log(student[i][j]);
+//     }
+// }
+
+
+// let fruits=["apple","mango","banana","litchi","papaya"];
+
+// for(fruit of fruits){
+//     console.log(fruit);
+// }
+
+// for(name of "lakhanprajapati"){
+//     console.log(name);
+// }
+
+
+// let heros=[['ironman', 'thor', 'spiderman'],['superman', 'wonderman', 'flash']];
+
+// for(list of heros){
+//     for(hero of list){
+//         console.log(hero);
+//     }
+// }
+
---
+++ Day_17/practice.js
@@ -0,0 +1,17 @@
+// // question no 1
+
+// let arr=[1, 2, 3, 4, 5, 6, 2, 3];
+// let num=2;
+
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]==num){
+//         arr.splice(i, 1);
+//     }
+// }
+// console.log(arr);
+
+// practice question 2
+
+let num =3488943949;
+
+console.log(num.length())
