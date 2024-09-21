d89b1d360495b75fef559fd0e8936eabf0f2daca
Lakhan Prajapat <lakhan10209@gmail.com>
2024-09-21 19:52:29 +0530

    Add files via upload

---
+++ Day_18/app.js
@@ -0,0 +1,114 @@
+console.log("Day_18");
+
+// // objects literals.........
+// const student={
+//     name :"lakhan",
+//     roll_no: 10622384,
+//     age:19,
+//     marks:99.98
+// };
+// console.log(student); 
+
+
+// //thread & twiter post.........
+// const post={
+//     username:"@its_lakhan.145",
+//     content:"this is new jurney of success",
+//     likes: 397,
+//     repost:35,
+//     tags:["@amit", "aakash", "ajay"]
+// };
+// console.log(post);
+
+// // add & update values.....
+// const student={
+//     name :"lakhan",
+//     roll_no: 10622384,
+//     age:19,
+//     marks:99.98,
+//     city:"mumbai"
+// };
+
+// student.city="delhi";
+// student.marks="A";
+// student.gender="male";
+// console.log(student)
+
+
+// // nested Objects.......
+// const classInfo={
+//     lakhan:{
+//         grade:"A+",
+//         city:"Alwar"
+//     },
+//     aman:{
+//         grade:"A",
+//         city:"panipat"
+//     },
+//     sumit:{
+//         grade:"O",
+//         city:"Mumbai"
+//     }
+// };
+// console.log(classInfo);
+// classInfo.lakhan.city="delhi";
+// console.log(classInfo);
+
+
+// // Array of Objects.....
+// const classInfo=[
+//     {
+//         name:"lakhan",
+//         grade:"A+",
+//         city:"Alwar"
+//     },
+
+//     {
+//         name:"aman",
+//         grade:"A",
+//         city:"panipat"
+//     },
+
+//     {
+//         name:"karan",
+//         grade:"O",
+//         city:"Mumbai"
+//     }
+// ];
+
+// console.log(classInfo);
+
+
+// // genarate Random no.....
+// let otp=Math.floor( Math.random()*10000);
+
+
+// // practice qus 1.......
+// let random = Math.floor( Math.random()*100 )+1; 
+
+
+// // practice qus 2
+// let random = Math.floor( Math.random()*5 )+1; 
+
+
+// // valid otp enter..
+
+// const max=prompt("enter a max no you want to get otp");
+// const random = Math.floor( Math.random()*max )+1;
+// let guess = prompt("guess the correct random no ");
+
+// while(true){
+//     if(guess=="quit"){
+//         console.log("You are quit");
+//         break;
+//     }
+
+//     if(guess==random){
+//         console.log("you are enter succesfully random =", random)
+//         break;
+//     }
+//     else{
+//         guess=prompt("wrong guess please try again");
+//     }
+// }
+
---
+++ Day_18/practice.js
@@ -0,0 +1,24 @@
+// // practice qus 1......
+// let dice= Math.floor(Math.random()*6) +1;
+
+
+// // practice qus 2......
+// const car={
+//     name: "audi",
+//     model: "suv700",
+//     color : "jet black"
+// };
+
+// console.log( car.name );
+// console.log( car["name"] );
+
+
+//  // practice qus 2.....
+// const person={
+//     name : "lakhan",
+//     age : 18,
+//     city : "delhi"
+// };
+// person.city="new york";
+// person.contury="United State";
+// console.log( person );
