2271d3d578f2742c6fcb86737299cc6828b6c369
lakhanprajapat1 <lakhan10209@gmail.com>
2024-10-24 20:38:47 +0530

    add new

---
+++ Day_23/script.js
@@ -0,0 +1,29 @@
+// creat nth number array and sum of all
+
+//create array nth number
+let n= prompt("enter number");
+let arr =[];
+
+for(let i=1; i<=n; i++){
+    arr[i-1]=i;
+}
+console.log(arr);
+
+
+// sum of n number in array
+let sumOfArray=arr.reduce((a, b) => {
+    return a+b;
+})
+console.log("sum of numbers in a array = " + sumOfArray)
+
+
+
+// now calculate the nth nubers factorial 
+let factorial=arr.reduce((a, b) => {
+    return a*b;
+})
+console.log("factorial = " +factorial)
+
+
+
+// 
